66da03182dd20bcc22170604268bab0326693f69
Iyadi Cyril <iyadicy@gmail.com>
2021-11-02 23:24:35 +0100

    Folder containing all pages

---
+++ src/pages/BooksPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Books from '../components/Books';
+import AddBookForm from '../components/AddBookForm';
+
+const BooksPage = () => (
+  <div>
+    <Books />
+    <AddBookForm />
+  </div>
+);
+
+export default BooksPage;
