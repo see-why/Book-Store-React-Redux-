03c5e7237d593a512a1c26dd980bc8d6f6f304e9
Iyadi Cyril <iyadicy@gmail.com>
2021-11-03 11:38:51 +0100

    Add onClick function

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,28 +1,36 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import propTypes from 'prop-types';
+import { removeBook } from '../redux/books/books';
 
 const BookCard = ({
   title,
   author,
   category,
   completed,
-}) => (
-  <>
-    <ul>
-      <li>{category}</li>
-      <li>{title}</li>
-      <li>{author}</li>
-      <li>{completed}</li>
-    </ul>
-    <button type="button">Remove</button>
-  </>
-);
+  id,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <ul id={id}>
+        <li>{category}</li>
+        <li>{title}</li>
+        <li>{author}</li>
+        <li>{completed}</li>
+      </ul>
+      <button type="button" onClick={() => { dispatch(removeBook(id)); }}>Remove</button>
+    </>
+  );
+};
 
 BookCard.propTypes = {
   title: propTypes.string.isRequired,
   author: propTypes.string.isRequired,
   category: propTypes.string.isRequired,
   completed: propTypes.string.isRequired,
+  id: propTypes.string.isRequired,
 };
 
 export default BookCard;
